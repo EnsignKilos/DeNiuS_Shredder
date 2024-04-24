5f24ffb6773a453b47736a83a293e6b39b8361d1
ensignkilos <ginksontesting@outlook.com>
2024-04-24 09:11:03 +0100

    Refactor file input and validation in Program.cs

--- Shredder/Program.cs
+++ Shredder/Program.cs
@@ -1,4 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 string filePath = args[0];
 DNSListFileReader DNSFileRead = new DNSListFileReader();
-HashSet<string> lines = DNSFileRead.ReadFile(filePath);
+ConcurrentQueue<string> lines = DNSFileRead.LoadFile(filePath);
--- Shredder/ReadDNSInputFile.cs
+++ Shredder/ReadDNSInputFile.cs
@@ -1,23 +1,42 @@
 public class DNSListFileReader
 {
-    public HashSet<string> ReadFile(string filePath)
+    public ImmutableHashSet<string>? FileHashSet { get; private set; }
+    public ConcurrentQueue<string>? FileQueue { get; private set; }
+
+    public ConcurrentQueue<string> LoadFile(string filePath)
     {
-        HashSet<string> lines = [];
-        if (File.Exists(filePath) && new FileInfo(filePath).Length > 0)
+        FileHashSet = CreateFileHashSet(filePath);
+        return CreateMultithreadedQueue(FileHashSet);
+    }
+
+    private ImmutableHashSet<string> CreateFileHashSet(string filePath)
+    {
+        var builder = ImmutableHashSet.CreateBuilder<string>();
+        if (!File.Exists(filePath))
+        {
+            throw new FileNotFoundException("File not found", filePath);
+        }
+        else if (new FileInfo(filePath).Length == 0)
+        {
+            throw new FileNotFoundException("File is empty", filePath);
+        }
+        else
         {
             using (StreamReader reader = new(filePath))
             {
-                string line;
+                string? line;
                 while ((line = reader.ReadLine()) != null)
                 {
-                    lines.Add(line);
+                    builder.Add(line);
                 }
             }
         }
-        else 
-        {
-            throw new FileNotFoundException("File not found or empty", filePath);
-        }
-        return lines;
+        return builder.ToImmutable();
+    }
+
+    private ConcurrentQueue<string> CreateMultithreadedQueue(ImmutableHashSet<string> Entries)
+    {
+        ConcurrentQueue<string> lines = new ConcurrentQueue<string>(Entries);
+        return FileQueue = lines;
     }
 }
